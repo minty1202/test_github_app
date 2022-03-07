674fe6ab3c9d477dcf3ebce4cbe633fceeddb613
minty1202 <aki.men1202@gmail.com>
2022-03-07 17:38:48 +0900

    [add] puts

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
+  puts 'xxxxxxxxxxxxxxxxxxxx'
+  puts 'xxxxxxxxxxxxxxxxxxxx'
+  puts 'xxxxxxxxxxxxxxxxxxxx'
   p params
+  puts 'xxxxxxxxxxxxxxxxxxxx'
+  puts 'xxxxxxxxxxxxxxxxxxxx'
+  puts 'xxxxxxxxxxxxxxxxxxxx'
   'Hello'
 end
